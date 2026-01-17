68eca6dd2783a25048b761b0f842babfd488b381
Pexaccount <pwwanghe@gmail.com>
2026-01-17 15:46:43 +0800

    Create firebase.js

---
+++ pages/api/firebase.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  const target = "https://pexchatweb-default-rtdb.firebaseio.com";
+  const path = req.url.replace(/^\/api\/firebase/, "");
+  const url = target + path;
+
+  const response = await fetch(url, {
+    method: req.method,
+    headers: {
+      ...req.headers,
+      host: "pexchatweb-default-rtdb.firebaseio.com"
+    },
+    body: req.method === "GET" ? null : await req.text(),
+  });
+
+  res.status(response.status);
+  response.headers.forEach((v, k) => res.setHeader(k, v));
+  const data = await response.arrayBuffer();
+  res.send(Buffer.from(data));
+}
